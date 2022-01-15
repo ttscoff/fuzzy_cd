d645d06598b73820504e2703edefcc36012d4a82
Brett Terpstra <me@brettterpstra.com>
2022-01-15 14:31:49 -0600

    LICENSE

--- conf.d/fuzzy_cd_common.fish
+++ conf.d/fuzzy_cd_common.fish
@@ -3,12 +3,14 @@ function fcd_install --on-event fuzzy_cd_install
 
 	if not test -d ~/.marks
 		fcd_warn -e "fuzzy_cd: jump doesn't appear to be installed. Please see https://github.com/oh-my-fish/plugin-jump"
+		fcd_warn "......... if you have oh-my-fish installed, you can use `omf install jump`"
 	else
 		fcd_warn "......... jump is installed"
 	end
 
 	if not test (which fasd)
 		fcd_warn -e "fuzzy_cd: fasd is not installed/available in PATH. Please install https://github.com/clvv/fasd"
+		fcd_warn "......... if you're on a Mac and have Homebrew installed, you can use `brew install fasd`"
 	else
 		fcd_warn "......... fasd is available"
 	end
