660eb5159ae20b9948e4878702a72c01baa3aa57
Brett Terpstra <me@brettterpstra.com>
2022-01-15 14:47:35 -0600

    fix error on update

--- conf.d/fuzzy_cd.fish
+++ conf.d/fuzzy_cd.fish
@@ -2,9 +2,8 @@ if functions -q __fuzzy_cd
 	if test -d ~/.marks && test (which fasd)
 		if not functions -q __fuzzy_wrapped_cd
 			functions -c cd __fuzzy_wrapped_cd
-			functions -e cd
 		end
-
+		functions -e cd
 		functions -c __fuzzy_cd cd
 	end
 end
